b76792a17116c8d4a1388795b2e169ac3fda111e
mizuki murata <mizucmke@gmail.com>
2024-06-10 14:16:56 +0900

    変更

--- branch-practice.php
+++ branch-practice.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <title>WEB開発7回目</title>
-        <link href="./style.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         ブランチ作成練習中
